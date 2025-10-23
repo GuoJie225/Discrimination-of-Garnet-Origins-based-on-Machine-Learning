dc32dcba857d37c1e02f9c66a93d0ff0449839ae
GuoJie225 <Gorgeous_Guojie@163.com>
2025-10-23 21:07:41 +0800

    GarnetOrigin.py

--- GarnetOrigin.py
+++ GarnetOrigin.py
@@ -131,7 +131,8 @@ if st.button('Make predictions') and st.session_state.uploaded_file is not None:
     plt.title('Distribution of Predictions')
     st.pyplot(fig)
 elif st.session_state.uploaded_file is None:
-    st.text('Please input your data.')
+    st.markdown(':red [Please input your data.]')
+
 
 
 
