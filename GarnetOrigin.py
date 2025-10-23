a2ec1c3e59efc2ece9ba6114728461239a44cc0b
GuoJie225 <Gorgeous_Guojie@163.com>
2025-10-23 20:36:25 +0800

    GarnetOrigin.py

--- GarnetOrigin.py
+++ GarnetOrigin.py
@@ -130,5 +130,6 @@ if st.button('Make predictions') and st.session_state.uploaded_file is not None:
     sns.countplot(data=st.session_state.data, x='prediction', hue='Sample')
     plt.title('Distribution of Predictions')
     st.pyplot(fig)
-elif st.button('Make predictions', key="make_predictions_button_2") and st.session_state.data is None:
+elif st.button('Make predictions', key="make_predictions_button_1") and st.session_state.data is None:
     st.text('Please input your data.')
+
