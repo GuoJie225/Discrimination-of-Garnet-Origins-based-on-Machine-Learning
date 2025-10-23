9018c7bde8c8348b18ac4567013035c1287e5c7f
GuoJie225 <Gorgeous_Guojie@163.com>
2025-10-23 20:29:41 +0800

    GarnetOrigin.py

--- GarnetOrigin.py
+++ GarnetOrigin.py
@@ -17,7 +17,9 @@ if 'uploaded_file' not in st.session_state:
     st.session_state.uploaded_file = None
 if 'data' not in st.session_state:
     st.session_state.data = None
-    
+if 'prediction_made' not in st.session_state:
+    st.session_state.prediction_made = False
+
 global data
     
 @st.cache_data
@@ -51,14 +53,19 @@ st.divider()
 
 st.header('2. Upload your data')
 
-st.session_state.uploaded_file = st.file_uploader("Garnet composition dataset:", type=['xlsx', 'csv', 'xls'], accept_multiple_files=False)
+uploaded_file = st.file_uploader("Garnet composition dataset:", type=['xlsx', 'csv'], accept_multiple_files=False)
 
-if st.session_state.uploaded_file is not None:
-    if st.session_state.uploaded_file.name.split('.')[-1] == 'xlsx':
-        st.session_state.data = pd.read_excel(st.session_state.uploaded_file)
-    else:
-        st.session_state.data = pd.read_csv(st.session_state.uploaded_file)
+if uploaded_file is not None:
+    if st.session_state.uploaded_file != uploaded_file:
+        st.session_state.uploaded_file = uploaded_file
 
+        if uploaded_file.name.split('.')[-1] == 'xlsx':
+            st.session_state.data = pd.read_excel(uploaded_file)
+        else:
+            st.session_state.data = pd.read_csv(uploaded_file)
+        st.session_state.prediction_made = False
+
+if st.session_state.data is not None:
     st.dataframe(st.session_state.data)
 
 st.divider()
@@ -88,21 +95,28 @@ def to_result_df(model):
     return result_df
 
 if st.button('Make predictions') and st.session_state.uploaded_file is not None:
-    st.session_state.data.fillna(0.001, inplace=True)
-    if model == "Major Elements":
-        scaled_data = scaler_major_model.transform(st.session_state.data.query('97.50 < Sum < 102.50').iloc[:,2:-1])
-        mask = (st.session_state.data['Sum'] > 97.50) & (st.session_state.data['Sum'] < 102.50)
-        st.session_state.data.loc[mask, 'prediction'] = xgboost_major_model.predict(scaled_data)
-    else:
-        scaled_data = scaler_trace_model.transform(np.log1p(st.session_state.data.iloc[:,2:]))
-        st.session_state.data['prediction'] = xgboost_trace_model.predict(scaled_data)
+    data = st.session_state.data
+    data.fillna(0.001, inplace=True)
 
-    st.session_state.data['prediction'].replace({0:'Igneous', 1:'Metamorphic', 2:'Peritectic'}, inplace=True)
+    if 'Sum' in data.columns:
+        if model == "Major Elements":
+
+            scaled_data = scaler_major_model.transform(data.query('97.50 < Sum < 102.50').iloc[:, 2:-1])
+            mask = (data['Sum'] > 97.50) & (data['Sum'] < 102.50)
+            data.loc[mask, 'prediction'] = xgboost_major_model.predict(scaled_data)
+        else:
+            scaled_data = scaler_trace_model.transform(np.log1p(data.iloc[:,2:]))
+            data['prediction'] = xgboost_trace_model.predict(scaled_data)
+    else:
+            st.error("Data should include the 'Sum' column")
+    data['prediction'].replace({0:'Igneous', 1:'Metamorphic', 2:'Peritectic'}, inplace=True)
+
+    st.session_state.data = data
+    st.session_state.prediction_made = True
+
+    st.dataframe(data)
 
     result_df = to_result_df(model)
-
-    st.dataframe(st.session_state.data)
-
     st.download_button(
         label = "Download",
         data = result_df,
@@ -111,9 +125,8 @@ if st.button('Make predictions') and st.session_state.uploaded_file is not None:
     )
 
     fig = plt.figure(dpi=600)
-    sns.countplot(data=data, x='prediction', hue='Sample')
+    sns.countplot(data=st.session_state.data, x='prediction', hue='Sample')
     plt.title('Distribution of Predictions')
     st.pyplot(fig)
-else:
+elif st.button('Make predictions') and st.session_state.data is None:
     st.text('Please input your data.')
-
